400b123fc8c53b7b7f99198b3e59a98cfa91de23
zhiqiang <simlesos@gmail.com>
2020-09-02 14:50:41 +0800

    fix: 🐛 修正生产环境构建无法结束进程的问题

--- index.js
+++ index.js
@@ -14,30 +14,47 @@ module.exports = (api, options) => {
   }
 
   api.chainWebpack((webpackConfig) => {
-    let isReady = false
+    webpackConfig.when(
+      process.env.NODE_ENV === 'development',
+      (config) => {
+        let isReady = false
 
-    // prettier-ignore
-    webpackConfig
-      .plugin('chokidar')
-        .use(new webpackChokidarPlugin({
-          watchFilePaths: [
-            'src/views/**/*.vue', 'src/view/**/*.ts'
-          ], 
-          onReadyCallback: () => {
-            isReady = true
-          }, 
-          onChangeCallback: () => { return null },
-          onAddCallback: () => {
-            if (isReady) {
+        config.plugin('chokidar').use(
+          new webpackChokidarPlugin({
+            watchFilePaths: ['src/views/**/*.vue', 'src/view/**/*.ts'],
+            onReadyCallback: () => {
+              isReady = true
+            },
+            onChangeCallback: () => {
+              return null
+            },
+            onAddCallback: () => {
+              if (isReady) {
+                generatorRouter(pluginOptions)
+              }
+            },
+            onUnlinkCallback: () => {
               generatorRouter(pluginOptions)
-            }
+            },
+            usePolling: false,
+            ignored: '/node_modules/',
+          })
+        )
+      },
+      (config) => {
+        config.plugin('vue-auto-routing').use({
+          apply(compiler) {
+            compiler.hooks.thisCompilation.tap('VueAutoRoutingPlugin', (compilation) => {
+              try {
+                generatorRouter(pluginOptions)
+              } catch (error) {
+                compilation.errors.push(error)
+              }
+            })
           },
-          onUnlinkCallback: () => {
-            generatorRouter(pluginOptions)
-          },
-          usePolling: false,
-          ignored: '/node_modules/',
-      }))
+        })
+      }
+    )
 
     // prettier-ignore
     webpackConfig.module
@@ -45,7 +62,7 @@ module.exports = (api, options) => {
       .post()
       .resourceQuery(/blockType=route-meta/)
       .use('route-meta')
-        .loader('vue-cli-plugin-auto-routing/route-loader')
+      .loader('vue-cli-plugin-auto-routing/route-loader')
 
     // prettier-ignore
     webpackConfig.module
@@ -53,6 +70,6 @@ module.exports = (api, options) => {
       .post()
       .resourceQuery(/blockType=route/)
       .use('route')
-        .loader('vue-cli-plugin-auto-routing/route-loader')
+      .loader('vue-cli-plugin-auto-routing/route-loader')
   })
 }
